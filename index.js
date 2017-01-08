b4a2b978efcf3abb9e1443cd66457aa18ab59c75
Yuya TAMANO <everfree.main@gmail.com>
2017-01-08 21:23:27 +0900

    Built first window sample.

---
+++ index.js
@@ -0,0 +1,16 @@
+'use strict';
+
+const electron = require('electron');
+const app = electron.app;
+const BrowserWindow = electron.BrowserWindow;
+
+let mainWindow;
+
+app.on('ready', function() {
+  mainWindow = new BrowserWindow({width: 600, height: 480});
+  mainWindow.loadURL('file://' + __dirname + '/index.html');
+  mainWindow.webContents.openDevTools();
+  mainWindow.on('closed', function() {
+    mainWindow = null;
+  });
+});
