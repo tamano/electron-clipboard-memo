dec56162651b65df2f12009d0e19a28c28c94db5
Yuya TAMANO <everfree.main@gmail.com>
2017-02-12 23:26:09 +0900

    Refactor: Change file names

--- app.js
+++ app.js
@@ -8,7 +8,7 @@ let mainWindow;
 
 app.on('ready', function() {
   mainWindow = new BrowserWindow({titleBarStyle: 'hidden', width: 600, height: 480});
-  mainWindow.loadURL('file://' + __dirname + '/index.html');
+  mainWindow.loadURL('file://' + __dirname + '/main.html');
   mainWindow.webContents.openDevTools();
   mainWindow.on('closed', function() {
     mainWindow = null;
