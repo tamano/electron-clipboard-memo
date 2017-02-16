8d4daf96563a180dee50551a2c918f1e71d2d18c
Yuya TAMANO <everfree.main@gmail.com>
2017-02-16 20:54:51 +0900

    Add TODO comments

--- app.js
+++ app.js
@@ -9,7 +9,10 @@ let mainWindow;
 app.on('ready', function() {
   mainWindow = new BrowserWindow({titleBarStyle: 'hidden', width: 600, height: 480});
   mainWindow.loadURL('file://' + __dirname + '/main.html');
+
+  //TODO: 開発時のみ
   mainWindow.webContents.openDevTools();
+
   mainWindow.on('closed', function() {
     mainWindow = null;
   });
